bbb6c341a15e6e26b85ad23fcd061a5edeb1c637
laomiXD <454182465@qq.com>
2018-09-21 20:24:29 +0800

    fetch: 将生成的题目和标准答案写入到当前目录文件中 fetch: 将用户输入的答案文件与标准答案文件比对统计正确与错误

--- src/com/laomi/ExcersiceBook.java
+++ src/com/laomi/ExcersiceBook.java
@@ -1,12 +1,14 @@
 package com.laomi;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ExcersiceBook {
+
+
+    private static String timestamp = String.valueOf(System.currentTimeMillis());
 
     public static File CopyProblems(List<Expression> expressions) {
 
@@ -15,11 +17,13 @@ public class ExcersiceBook {
             Expression e = expressions.get(i);
             stringBuilder.append(""+(i+1)+". "+e + " ="+"\n");
         }
+
+
         String input = stringBuilder.toString();
-        File Excersices = new File(System.getProperty("user.dir") + "/" + "Exercises.txt");
+        File Excersices = new File(System.getProperty("user.dir") + "/" + "Exercises_"+timestamp+".txt");
         try {
             PrintStream ps = new PrintStream(new FileOutputStream(Excersices));
-            ps.append(input);// 在已有的基础上添加字符串
+            ps.append(input);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             }
@@ -33,8 +37,9 @@ public class ExcersiceBook {
             Expression e = expressions.get(i);
             stringBuilder.append(""+(i+1)+". "+e.getUltimateAnswer()+"\n");
         }
+
         String input = stringBuilder.toString();
-        File Answer = new File(System.getProperty("user.dir") + "/" + "Answer.txt");
+        File Answer = new File(System.getProperty("user.dir") + "/" + "Answer_"+timestamp+".txt");
         try {
             PrintStream ps = new PrintStream(new FileOutputStream(Answer));
             ps.append(input);// 在已有的基础上添加字符串
@@ -44,10 +49,64 @@ public class ExcersiceBook {
         return Answer;
     }
 
-    public static void CheckTheAnswer(String excercisepath,String answerpath)
-    {
-        File excercises = new File(excercisepath);
-        File answer = new File(answerpath);
+    public static void CheckTheAnswer(String excersicepath,String useranswerpath) throws IOException {
 
+        List<String> Correct = new ArrayList<>();
+        List<String> False = new ArrayList<>();
+        File excersice = new File(excersicepath);
+        String answer_timestamp = excersicepath.substring(excersicepath.lastIndexOf("_")+1,excersicepath.lastIndexOf("."));
+
+        File answer = new File(System.getProperty("user.dir") + "/" + "Answer_"+answer_timestamp+".txt");
+
+        try{
+
+            FileReader readuser = new FileReader(new File(useranswerpath));
+            FileReader readanswer = new FileReader(answer);
+            BufferedReader bufferuser = new BufferedReader(readuser);
+            BufferedReader bufferanswer = new BufferedReader(readanswer);
+            String s_answer = null;
+            String s_user = null;
+            while((s_answer=bufferanswer.readLine())!=null&&(s_user=bufferuser.readLine())!=null)
+            {
+                if(s_answer.equals(s_user))
+                {
+                    Correct.add(s_answer.substring(0,s_answer.lastIndexOf(".")));
+                }
+                else
+                {
+                    False.add(s_answer.substring(0,s_answer.lastIndexOf(".")));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File Grade = new File(System.getProperty("user.dir")+"/"+"Grade"+".txt");
+        StringBuilder s_grade = new StringBuilder("Correct: ");
+        s_grade.append(Correct.size()+"(");
+        for(int i=0;i<Correct.size();i++)
+        {
+            s_grade.append(Correct.get(i)+",");
+        }
+        s_grade.append(")"+"\n");
+
+        s_grade.append("Wrong: ");
+        s_grade.append(False.size()+"(");
+
+        for(int i=0;i<False.size();i++)
+        {
+            s_grade.append(False.get(i)+",");
+        }
+        s_grade.append(")"+"\n");
+
+        String tograde = s_grade.toString();
+        try {
+            PrintStream ps = new PrintStream(new FileOutputStream(Grade));
+            ps.append(tograde);// 在已有的基础上添加字符串
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
 }
--- src/com/laomi/Test.java
+++ src/com/laomi/Test.java
@@ -1,5 +1,6 @@
 package com.laomi;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ThreadLocalRandom;
@@ -9,8 +10,10 @@ import java.util.concurrent.ThreadLocalRandom;
  * @since 2018-09-19 14:20
  **/
 public class Test {
-    public static void main(String[] args) {
-        Number n = 3.0;
-        System.out.println(n);
+    public static void main(String[] args) throws IOException {
+
+        ExcersiceBook.CheckTheAnswer("F:\\arithmetic_producer\\Exercises_1537530984178.txt," ,
+                "F:\\arithmetic_producer\\test.txt");
+
     }
 }
